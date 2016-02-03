3e42740a9893170160e6656cd0513ecd476e4aad
Eunhyang Kwon <ehkwon1011@gmail.com>
2016-02-03 14:57:24 +0900

    Programming with R Week3 Quiz2

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
+## 'makeCacheMatrix' creates a special "matrix", which is really a list
+## containing a function to
+##  1. set the matrix
+##  2. get the matrix
+##  3. set the inverse
+##  4. get the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+				m <- NULL ## Initialize to NULL
+			
+				## '<<-' used to assign a value to an object in an environment
+				## that is different from the current environment.
+				## Create the matrix in the current environment
+				set <- function(y) {
+								x <<- y 
+								m <<- NULL
+				}
+				## Get the value of the matrix
+			  get <- function() x
+			  ## Inverse the matrix
+				setMatrix <- function(inverse) m <<- inverse
+				## Get the inversed matrix
+				getInverse <- function() m
+				## Return the created functions
+				list(set = set, get = get,
+						 setMatrix = setMatrix,
+						 getInverse = getInverse) 
 
 }
 
@@ -12,4 +37,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()
+				
+        ## If the matrix has already been calculated 
+				if(!is.null(m)) {
+								message("getting cached data")
+								return(m)
+				}
+        ## Get the matrix stored with makeCacheMatrix
+        matrix <- x$get()
+				m <- solve(matrix, ...)
+				## Set inverted matrix 
+				x$setMatrix(m)
+				return(m)
 }
